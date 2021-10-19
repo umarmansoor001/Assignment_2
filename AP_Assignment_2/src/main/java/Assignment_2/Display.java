878730b32687cd01ba66f0c5fc0ddcd8c6b8fd68
umarmansoor001 <i190594@nu.edu.pk>
2021-10-19 20:22:21 +0500

    commit

---
+++ AP_Assignment_2/src/main/java/Assignment_2/Display.java
@@ -0,0 +1,9 @@
+package Assignment_2;
+
+public class Display {
+
+	static void Greetings()
+	{
+		System.out.println("Welcome to Flight reservation system");
+	}
+}
---
+++ AP_Assignment_2/src/main/java/Assignment_2/Main.java
@@ -0,0 +1,13 @@
+package Assignment_2;
+
+//Driver class
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display d=new Display();
+		d.Greetings();
+	}
+
+}
